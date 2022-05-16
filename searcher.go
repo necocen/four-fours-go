276a6e80f62af8554de810f456db5810d41492fb
necocen <necocen@gmail.com>
2022-05-17 00:09:13 +0900

    using goroutines

--- searcher.go
+++ searcher.go
@@ -1,6 +1,10 @@
 package fourfours
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"sync/atomic"
+)
 
 type Searcher struct {
 	memo      map[string]*map[float64]Equation
@@ -14,6 +18,22 @@ func NewSearcher(unaryOps []UnaryOp, binaryOps []BinaryOp) Searcher {
 		unaryOps:  unaryOps,
 		binaryOps: binaryOps,
 	}
+}
+
+type unaryOpTask struct {
+	UnaryOp
+	Equation
+}
+
+type binaryOpTask struct {
+	BinaryOp
+	lhs Equation
+	rhs Equation
+}
+
+type taskResult struct {
+	Equation
+	ok bool
 }
 
 func (s *Searcher) Search(key string) *map[float64]Equation {
@@ -36,16 +56,52 @@ func (s *Searcher) Search(key string) *map[float64]Equation {
 		knowledgeLeft := *s.Search(keyLeft)
 		knowledgeRight := *s.Search(keyRight)
 		fmt.Printf("Combining %s and %s\n", keyLeft, keyRight)
-		for _, op := range s.binaryOps {
-			for _, el := range knowledgeLeft {
-				for _, er := range knowledgeRight {
-					newEq, ok := op.Apply(&el, &er)
-					if !ok {
-						continue
+		tasks := make(chan binaryOpTask)
+		results := make(chan taskResult)
+		done := make(chan struct{})
+		var counter int64
+
+		// spawn workers
+		for i := 0; i < runtime.NumCPU(); i++ {
+			go func(tasks <-chan binaryOpTask, results chan<- taskResult) {
+				for task := range tasks {
+					newEq, ok := task.BinaryOp.Apply(&task.lhs, &task.rhs)
+					results <- taskResult{
+						Equation: newEq,
+						ok:       ok,
 					}
-					if oldEq, ok := knowledge[newEq.value]; !ok || oldEq.cost > newEq.cost {
-						knowledge[newEq.value] = newEq
+				}
+			}(tasks, results)
+		}
+
+		// put jobs
+		go func() {
+			for _, op := range s.binaryOps {
+				for _, el := range knowledgeLeft {
+					for _, er := range knowledgeRight {
+						atomic.AddInt64(&counter, 1)
+						tasks <- binaryOpTask{BinaryOp: op, lhs: el, rhs: er}
 					}
+				}
+			}
+			close(done)
+			close(tasks)
+		}()
+
+	WaitBinaryOp:
+		// wait for results
+		for {
+			select {
+			case result := <-results:
+				if result.ok {
+					if oldEq, ok := knowledge[result.value]; !ok || oldEq.cost > result.cost {
+						knowledge[result.value] = result.Equation
+					}
+				}
+				atomic.AddInt64(&counter, -1)
+			case <-done:
+				if counter == 0 {
+					break WaitBinaryOp
 				}
 			}
 		}
@@ -57,14 +113,50 @@ func (s *Searcher) Search(key string) *map[float64]Equation {
 			oldKnowledge[k] = v
 		}
 		fmt.Printf("Applying Unary Ops to %s (%d/3)\n", key, i+1)
-		for _, op := range s.unaryOps {
-			for _, e := range oldKnowledge {
-				newEq, ok := op.Apply(&e)
-				if !ok {
-					continue
+		tasks := make(chan unaryOpTask)
+		results := make(chan taskResult)
+		done := make(chan struct{})
+		var counter int64
+
+		// spawn workers
+		for i := 0; i < runtime.NumCPU(); i++ {
+			go func(tasks <-chan unaryOpTask, results chan<- taskResult) {
+				for task := range tasks {
+					newEq, ok := task.UnaryOp.Apply(&task.Equation)
+					results <- taskResult{
+						Equation: newEq,
+						ok:       ok,
+					}
 				}
-				if oldEq, ok := knowledge[newEq.value]; !ok || oldEq.cost > newEq.cost {
-					knowledge[newEq.value] = newEq
+			}(tasks, results)
+		}
+
+		// put jobs
+		go func() {
+			for _, op := range s.unaryOps {
+				for _, e := range oldKnowledge {
+					atomic.AddInt64(&counter, 1)
+					tasks <- unaryOpTask{UnaryOp: op, Equation: e}
+				}
+			}
+			close(done)
+			close(tasks)
+		}()
+
+	WaitUnaryOp:
+		// wait for results
+		for {
+			select {
+			case result := <-results:
+				if result.ok {
+					if oldEq, ok := knowledge[result.value]; !ok || oldEq.cost > result.cost {
+						knowledge[result.value] = result.Equation
+					}
+				}
+				atomic.AddInt64(&counter, -1)
+			case <-done:
+				if counter == 0 {
+					break WaitUnaryOp
 				}
 			}
 		}
